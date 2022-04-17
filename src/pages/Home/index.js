e759bd474cdaeb2bc368ea97e3c2ab9263abd33e
gwebeer <weber_guilherme@outlook.com>
2022-04-17 00:56:29 -0300

    Configurações do projeto e desenvolvimento do CRUD de usuários

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Register from './Register';
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div className='home'>
+                <Register />
+            </div>
+
+        )
+    }
+}
+export default Home
