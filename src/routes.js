09487fe3f724b5d33308c3d5ed19a41995b60aaa
Guilherme Weber <70821193+gwebeer@users.noreply.github.com>
2022-05-03 18:56:55 -0300

    Correção Rotas

--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,17 @@ import NewProject from './pages/NewProject'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
+import Dashboard from './pages/Dashboard'
+import EditProject from './pages/EditProject'
 
 const Rotas = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/project" element={<ProjectPage />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/project/:id" element={<ProjectPage />} />
+                <Route path="/editProject/:id" element={<EditProject />} />
                 <Route path="/newProject" element={<NewProject />} />
                 <Route path="/cadastro" element={<Register />} />
                 <Route path="/login" element={<Login />} />
