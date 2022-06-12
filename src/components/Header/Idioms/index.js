80ba6c45d150f668de129a1ab74e26418ccab4fb
yasminweber <43188652+yasminweber@users.noreply.github.com>
2022-06-11 21:08:05 -0300

    Alterações header e cadastro traduzido
    Alterações header e cadastro traduzido

--- src/components/Header/Idioms/index.js
+++ src/components/Header/Idioms/index.js
@@ -7,8 +7,8 @@ class HeaderIdioms extends Component {
         const t = translation(localStorage.getItem('language'));
         return (
             <NavDropdown className="align-self-center" title={<img className="flag-icon" src={t.flag} height={"30px"} alt={"something"} />} id="language">
-                <NavDropdown.Item onClick={changeLanguage} id="en"><img className="flag-icon" id="en" style={{paddingRight: "5px"}} src={translation('en').flag} height={"24px"} alt="USA Flag" />English</NavDropdown.Item>
-                <NavDropdown.Item onClick={changeLanguage} id="pt-br"><img className="flag-icon" id="pt-br" style={{paddingRight: "5px"}} src={translation('pt-br').flag} height={"24px"} alt="Brazil Flag" />Português</NavDropdown.Item>
+                <NavDropdown.Item onClick={changeLanguage} id="en"><img className="flag-icon" id="en" src={translation('en').flag} height={"24px"} alt="USA Flag" />English</NavDropdown.Item>
+                <NavDropdown.Item onClick={changeLanguage} id="pt-br"><img className="flag-icon" id="pt-br" src={translation('pt-br').flag} height={"24px"} alt="Brazil Flag" />Português</NavDropdown.Item>
             </NavDropdown>
         );
     }
