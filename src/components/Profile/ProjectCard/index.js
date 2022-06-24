e15d1087448b0c7235e74e54b07767a93326829b
Guilherme Weber <70821193+gwebeer@users.noreply.github.com>
2022-06-23 22:31:10 -0300

    finishing

--- src/components/Profile/ProjectCard/index.js
+++ src/components/Profile/ProjectCard/index.js
@@ -10,13 +10,13 @@ class ProfileProjectCard extends Component {
                     <div className='card-content'>
                         <div className='container-fluid px-0'>
                             <div className='row'>
-                                <div className='col-3 image-column'>
+                                <div className='col-3 d-md-block d-none image-column'>
                                     <div className='project-image'>
                                         <img className="profile-image" src="https://via.placeholder.com/130x130" alt="" />
                                     </div>
                                 </div>
 
-                                <div className='col-9 content-column px-4 pb-3'>
+                                <div className='col-md-9 col-12 content-column px-4 pb-3'>
                                     <div className="project-status text-end text-uppercase mt-1">{this.props.status}</div>
                                     <h4 className='project-name mt-1'> {this.props.projectName} </h4>
                                     <h5 className='project-cause mt-1'> {this.props.cause} </h5>
